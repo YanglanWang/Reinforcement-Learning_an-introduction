a3cff55ce72c5a13085b9e3d13b28a14c191a91e
YanglanWang <yanglan-17@mails.tsinghua.edu.cn>
2019-02-28 01:17:57 +0800

    complete

--- charpter2/figure_2.1_recurrence.py
+++ charpter2/figure_2.1_recurrence.py
@@ -5,13 +5,18 @@ r_whole=[]
 for e_tmp in e:
     r_sample = []
     r_tmp=[]
+    ll = 0
     for sample in range( 2000 ):
+        np.random.seed( ll )
+        ll=ll+1
         s_expected_update = dict()
         r_time=[]
         mu, sigma = 0, 1  # mean and standard deviation
         s_xing = np.random.normal( mu, sigma, 10 )
         s_expected = [0] * 10
         for t in range( 1000 ):
+            np.random.seed( ll )
+            ll = ll + 1
             e_random=np.random.rand()
             if e_tmp < e_random:
                 s_max_expected = max( s_expected )
@@ -29,6 +34,9 @@ for e_tmp in e:
             s_expected[index_max] = np.average( s_expected_update[index_max] )
             r_time.append(r)
         r_sample.append(r_time)
+    # for k in np.array(r_sample)[:,0,0]:
+    #     f.write(str(k)+'\n')
+    # f.close()
     r_tmp=np.average( r_sample, axis = 0 )
     r_line=[]
     for i in range(len(r_tmp)):
