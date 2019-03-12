d51715dc2fa05f04e1064c54c179a8674fabbdc2
YanglanWang <yanglan-17@mails.tsinghua.edu.cn>
2019-03-12 12:39:46 +0800

    3.12

--- charpter4/Gambler.py
+++ charpter4/Gambler.py
@@ -15,7 +15,7 @@ def Gambler(n):
             action_reward=[]
             for action_tmp in action:
                 if state_tmp+action_tmp<100:
-                    if state_tmp+1-action_tmp>0:
+                    if state_tmp-action_tmp>0:
                         v_action_tmp=p*states[state_tmp+action_tmp]+(1-p)*states[state_tmp-action_tmp]
                     else:
                         v_action_tmp = p * states[state_tmp  + action_tmp] + (1 - p) * states[0]
@@ -34,32 +34,31 @@ def Gambler(n):
     # states=states_new
         whole_test.append(list(states))
 
-    # for state_tmp in range( 1, len( states ) - 1 ):
-    #     action = np.arange( 1, min( state_tmp, 100 - state_tmp ) + 1 )
-    #     action_reward = []
-    #     for action_tmp in action:
-    #         if state_tmp + action_tmp < 100:
-    #             if state_tmp + 1 - action_tmp > 0:
-    #                 v_action_tmp = p * states[state_tmp + action_tmp] + (1 - p) * states[state_tmp - action_tmp]
-    #             else:
-    #                 v_action_tmp = p * states[state_tmp + action_tmp] + (1 - p) * states[0]
-    #         else:
-    #             if state_tmp - action_tmp > 0:
-    #                 v_action_tmp = p * (states[100] + 1) + (1 - p) * states[state_tmp - action_tmp]
-    #             else:
-    #                 v_action_tmp = p * (states[100] + 1) + (1 - p) * states[0]
-    #
-    #         action_reward.append( v_action_tmp )
-    #     state_action_max_set = [action[i] for i, j in enumerate( action_reward ) if j == max( action_reward )]
-    #     action_greedy[state_tmp] = state_action_max_set[0]
+    for state_tmp in range( 1, len( states ) - 1 ):
+        action = np.arange( 1, min( state_tmp, 100 - state_tmp ) + 1 )
+        action_reward = []
+        for action_tmp in action:
+            if state_tmp + action_tmp < 100:
+                if state_tmp + 1 - action_tmp > 0:
+                    v_action_tmp = p * states[state_tmp + action_tmp] + (1 - p) * states[state_tmp - action_tmp]
+                else:
+                    v_action_tmp = p * states[state_tmp + action_tmp] + (1 - p) * states[0]
+            else:
+                if state_tmp - action_tmp > 0:
+                    v_action_tmp = p * (states[100] + 1) + (1 - p) * states[state_tmp - action_tmp]
+                else:
+                    v_action_tmp = p * (states[100] + 1) + (1 - p) * states[0]
 
+            action_reward.append( v_action_tmp )
+        state_action_max_set = [action[i] for i, j in enumerate( np.round(action_reward,5) ) if j == max( np.round(action_reward,5) )]
+        action_greedy[state_tmp] = state_action_max_set[0]
 
     return whole_test,action_greedy
 
 if __name__=='__main__':
     whole_test,action_greedy=Gambler(100)
     fig,(axes1,axes2)=plt.subplots(2,1)
-    for i in range(len(whole_test)):
-        axes1.plot(whole_test[i][1:len(whole_test[i])-1])
+    # for i in range(len(whole_test)):
+    axes1.plot(whole_test[-1][1:len(whole_test[-1])-1])
     axes2.plot(action_greedy[1:100])
     plt.show()
